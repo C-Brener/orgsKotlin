c3b46a63e1320c5e6c971d5de91b5044051f38b6
brenermr <brenercaique0806@gmail.com>
2022-03-08 14:11:55 -0300

    Orgs App

--- app/src/main/java/com/ciandt/orgs/MainActivity.kt
+++ app/src/main/java/com/ciandt/orgs/MainActivity.kt
@@ -2,6 +2,7 @@ package com.ciandt.orgs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Toast
 import com.ciandt.orgs.databinding.ActivityMainBinding
 
@@ -12,5 +13,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.name.text = "Cesta de Frutas"
+        binding.description.text = "Laranja, manga e Uva"
+        binding.value.text = "$19.99"
     }
 }
